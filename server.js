249f86210e9df07adc324ed8fc6c1224d2fd3220
Anu-Codex <sarkaranubhav55@gmail.com>
2026-04-29 11:24:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const Player = require('./models/Player.js'); // Adjust the path to your file
+
 const bodyParser = require('body-parser');
 require('dotenv').config();
 
