d783014bf56b5052f0fbe1d4ca940a8bd77b7570
Anu-Codex <sarkaranubhav55@gmail.com>
2026-05-14 15:51:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ const nodemailer = require('nodemailer');
 // --- THE "BULLETPROOF" SMTP CONFIG ---
 const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
-    port: 465,
-    secure: true, // Use SSL
+    port: 587,
+    secure: false, // Use SSL
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
