765a5b4a6d0d81de0a082a4a97410a426efbac8d
Anu-Codex <sarkaranubhav55@gmail.com>
2026-06-19 14:14:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -188,6 +188,7 @@ const User = arenaConn.model('User', UserSchema);
 const Tournament = arenaConn.model('Tournament', TournamentSchema);
 const Activity = arenaConn.model('Activity', new mongoose.Schema({ text: String, date: { type: Date, default: Date.now } }));
 const OTP = arenaConn.model('OTP', new mongoose.Schema({ email: String, code: String, createdAt: { type: Date, default: Date.now, expires: 300 } }));
+const Prediction = arenaConn.model('Prediction', PredictionSchema);
 
 // Attached to NFA DB
 const Club = nfaConn.model('Club', ClubSchema);
